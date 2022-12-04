3ab2705779420766ae67f504074f9f35c79839a6
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-04 17:01:49 +0800

    fix: should copy .env files

--- start-servers.bat
+++ start-servers.bat
@@ -1,7 +1,8 @@
 @echo off
 
-cd /d %~dp0
+cd /d "%~dp0"
+cd dist
 
 powershell -Command "pm2 stop peer"
-powershell -Command "pm2 start dist/peer.js"
+powershell -Command "pm2 start peer.js"
 powershell -Command "pm2 save"
--- start-servers.sh
+++ start-servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/dist"
 
 pm2 stop peer
-pm2 start dist/peer.js
+pm2 start peer.js
 pm2 save
