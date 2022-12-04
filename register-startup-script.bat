8371259d8d77153825d3290925057b8df3fc7693
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-04 19:36:06 +0800

    fix: pm2 startup on windows

--- register-startup-script.bat
+++ register-startup-script.bat
@@ -1,10 +1,11 @@
 @echo off
 
 npm i -g yarn --registry=https://registry.npm.taobao.org
+npm i -g pm2 --registry=https://registry.npm.taobao.org
+npm i -g pm2-windows-startup --registry=https://registry.npm.taobao.org
 yarn config set registry https://registry.npm.taobao.org
 yarn install
 yarn run build
 
-powershell -c "pm2 startup"
-powershell
+powershell -c "pm2-startup install"
 pause
